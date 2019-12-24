e221084fb48ccd83012c54d95d4b59a3eaa08673
Sergey Smirnykh <antivirus147k@gmail.com>
2019-12-24 19:22:03 +0700

    Fix warnings

--- src/defines.rs
+++ src/defines.rs
@@ -29,6 +29,9 @@ pub const DEFINES: [Type; 3] = [INFINITY, -INFINITY, NAN];
 #[cfg(any(feature = "u32", feature = "u64"))]
 pub const DEFINES: [Type; 3] = [MAX, MAX - 1, MAX - 2];
 
+#[allow(dead_code)]
 pub const STACK_SIZE: usize = 1024 * 1024 * 256; // 256MB
+#[allow(dead_code)]
 pub const CHUNK_SIZE: usize = 1024 * 4; // 4KB
+#[allow(dead_code)]
 pub const SIZE: usize = std::mem::size_of::<Type>();
