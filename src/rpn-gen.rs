946f7c90fb02cae920ce983464c2eaadc6964172
Sergey Smirnykh <antivirus147k@gmail.com>
2019-12-24 22:03:46 +0700

    update readme; adjust generator to output exactly n numbers

--- src/rpn-gen.rs
+++ src/rpn-gen.rs
@@ -6,29 +6,25 @@ extern crate defines;
 use crate::defines::{ Type };
 
 fn main() {
-    let count = std::env::args().skip(1).next().expect("No args").parse::<isize>().expect("Can\'t parse");
+    let mut count = std::env::args().skip(1).next().expect("No args").parse::<usize>().expect("Can\'t parse");
     let mut rng = thread_rng();
 
     let mut terms: isize = 0;
-    for _ in 0..count {
-        if terms >= 2 {
-            let next: u8 = rng.gen();
-            if next > 128 {
-                let op = match next % 3 {
-                    0 => '+',
-                    1 => '-',
-                    2 => '*',
-                    _ => unsafe { std::hint::unreachable_unchecked() },
-                };
-                print!("{} ", op);
-                terms = terms - 1;
-            } else {
-                print!("{} ", rng.gen::<Type>().abs());
-                terms = terms + 1;
-            }
+    while count > 0 {
+        let next: u8 = rng.gen();
+        if next > 128 && terms >= 2 {
+            let op = match next % 3 {
+                0 => '+',
+                1 => '-',
+                2 => '*',
+                _ => unsafe { std::hint::unreachable_unchecked() },
+            };
+            print!("{} ", op);
+            terms = terms - 1;
         } else {
-            print!("{} ", rng.gen::<Type>().abs());  
+            print!("{} ", rng.gen::<Type>().abs());
             terms = terms + 1;
+            count = count - 1;
         }
     }
     while terms >= 2 {
@@ -40,9 +36,5 @@ fn main() {
         };
         print!("{} ", op);
         terms = terms - 1;
-    }
-    while terms < 1 {
-        print!("{} ", rng.gen::<Type>().abs());  
-        terms = terms + 1;
     }
 }
