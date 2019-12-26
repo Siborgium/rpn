29048dfe7558094c7fa14a5ea8f36684fc47f032
Sergey Smirnykh <antivirus147k@gmail.com>
2019-12-26 22:18:35 +0700

    use bufwriter to write bytes

--- src/rpn-encode.rs
+++ src/rpn-encode.rs
@@ -1,6 +1,6 @@
 
 use std::fs::File;
-use std::io::{ BufReader, BufRead, Write };
+use std::io::{ BufReader, BufRead, BufWriter, Write };
 
 extern crate defines;
 use crate::defines::{ Type, DEFINES };
@@ -10,15 +10,16 @@ fn main() -> std::io::Result<()> {
     let handle = stdin.lock();
     let reader = BufReader::new(handle);    
 
-    let mut file = File::create("input.txt")?;
+    let file = File::create("input.txt")?;
+    let mut writer = BufWriter::new(file);
     for v in reader.split(b' ') {
         let v = v.expect("Failed to split");
         let s = std::str::from_utf8(&v).expect("Not utf8").trim();
         if !s.is_empty() {
             if let Some((_, r)) = ["+", "-", "*"].iter().zip(DEFINES.iter()).find(|(e, _)| *e == &s) {
-                file.write(&r.to_ne_bytes()[..])?;
+                writer.write(&r.to_ne_bytes()[..])?;
             } else {
-                file.write(&s.parse::<Type>().expect("Can't parse").to_ne_bytes()[..])?;
+                writer.write(&s.parse::<Type>().expect("Can't parse").to_ne_bytes()[..])?;
             }
         }
     }
